15ba18ef2e6c47553e53110c8d9108977923306a
sldai <daishilong1236@gmail.com>
2024-05-23 16:53:00 +0800

    init

---
+++ common/src/terrain/site.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum SiteKindMeta {
+    Dungeon(DungeonKindMeta),
+    Cave,
+    Settlement(SettlementKindMeta),
+    Castle,
+    #[default]
+    Void,
+}
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum DungeonKindMeta {
+    Old,
+    Gnarling,
+    Adlet,
+    Haniwa,
+    SeaChapel,
+    Terracotta,
+    Cultist,
+}
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum SettlementKindMeta {
+    Default,
+    CliffTown,
+    DesertCity,
+    SavannahPit,
+    CoastalTown,
+}
